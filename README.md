597cc9c736adcd0de31509ed9d2d8c1cc2e463a6
yprime7 <ydevarshi@gmail.com>
2024-06-02 20:26:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Staking-Daily-Rewards
+ 
---
+++ utils/data.py
@@ -0,0 +1,15 @@
+# ADD YOUR PUBLIC ADDRESSES HERE
+def get_addresses():
+    return [
+        {"chain": "acala", "address": "ADD_YOUR_PUBLIC_ADDRESS_HERE"},
+        {"chain": "astar", "address": "ADD_YOUR_PUBLIC_ADDRESS_HERE"},
+        {"chain": "cardano", "address": "ADD_YOUR_PUBLIC_ADDRESS_HERE"},
+        {"chain": "moonbeam", "address": "ADD_YOUR_PUBLIC_ADDRESS_HERE"},
+        {"chain": "polkadot", "address": "ADD_YOUR_PUBLIC_ADDRESS_HERE"},
+    ]
+
+
+# REFERENCE CURRENCY
+def get_reference():
+    return 'USD'
+    
